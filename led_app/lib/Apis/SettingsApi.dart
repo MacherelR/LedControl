b9b1be87b22ab0f2cb97b6cc381d5292f1ee8ff3
RemyM <remymacherel98@gmail.com>
2022-12-01 16:55:26 +0100

    Refactoring, from scratch, proto + reqs.txt

--- led_app/lib/Apis/SettingsApi.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:led_app/Models/settings.dart';
-
-abstract class SettingsApi {
-  Future<void> deleteSettings(String id);
-  LedControllerSettings getSettings();
-  Future<void> saveSettings(LedControllerSettings tunerSettings);
-}
---
+++ led_app/lib/models/app_state.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+class AppState extends ChangeNotifier {}
